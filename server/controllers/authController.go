0e776d8084176bc8fbc0123e3e7d8ce4acb0ba03
shch989 <tankpop11@gmail.com>
2023-08-15 17:15:55 +0900

    Login 기능 완성

--- server/controllers/authController.go
+++ server/controllers/authController.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/gofiber/fiber/v2"
 	"github.com/shch989/React_Fiber_Login_App/database"
 	"github.com/shch989/React_Fiber_Login_App/models"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var SecretKey = "sceretkey"
 
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
@@ -29,4 +35,54 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{"message": "user not found"})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	expirationTime := time.Now().Add(time.Hour * 24)
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.Id)),
+		ExpiresAt: jwt.At(expirationTime),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "could not login",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  expirationTime,
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(token)
 }
